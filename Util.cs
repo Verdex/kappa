fca70668f91a48d72ca9b1fe3aa70c0b21457b7a
David L. Inman <davidlevere@gmail.com>
2017-12-21 21:19:55 -0500

    option type

---
+++ Util.cs
@@ -0,0 +1,40 @@
+
+
+namespace Kappa.Util
+{
+    public static class Option
+    {
+        public static IOption<T> Some<T>(T item)
+        {
+            return new Some<T>(item);
+        }
+        public static IOption<T> None<T>()
+        {
+            return new None<T>();
+        }
+    }
+    public interface IOption<T>
+    {
+        T Item { get; }
+        bool HasItem { get; }
+    }
+
+    public class Some<T> : IOption<T>
+    {
+        public T Item { get; set; }
+        public bool HasItem { get; } = true;
+        public Some(T item)
+        {
+            Item = item;
+        }
+    }
+
+    public class None<T> : IOption<T>
+    {
+        public T Item { get; }
+        public bool HasItem { get; } = false;
+        public None()
+        {
+        }
+    }
+}
