691789e65e8d51a256dd9a7e75fc7015dca4b48e
David L. Inman <davidlevere@gmail.com>
2018-01-02 21:33:45 -0500

    game state is a ref

--- Data/State.cs
+++ Data/State.cs
@@ -4,6 +4,6 @@ namespace Kappa.Data
     public class State
     {
         public Keyboard Keyboard { get; set; }
-        public GameMode GameMode { get; set; }
+        public Ref<GameMode> GameMode { get; set; }
     }
 }
